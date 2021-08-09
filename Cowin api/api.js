46bcc01fd3f3e1c9887083d084d9a59e00193f9b
talkshrey <talkshrey@gmail.com>
2021-08-09 14:54:07 +0530

    Task 3

--- Cowin api/api.js
+++ Cowin api/api.js
@@ -1,18 +1,16 @@
 var pincode = document.getElementById("pin");
 var url1 = ''
 
-function getStates() {
-    var myHeaders = new Headers();
-    myHeaders.append("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36");
+var myHeaders = new Headers();
+myHeaders.append("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36");
 
-    var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-    };
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
 
-    fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=110020&date=07-05-2021", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-}
+fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=110010&date=06-05-2021", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
